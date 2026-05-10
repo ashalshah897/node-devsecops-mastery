0d49c377f621f2978cf9e2f980c53643c9c055a9
ashalshah <ashalshah897@gmail.com>
2026-05-10 01:33:55 -0700

    Add pipeline instructions and break test

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,6 +5,6 @@ describe('GET /health', () => {
     it('should return status UP', async () => {
         const res = await request(app).get('/health');
         expect(res.statusCode).toEqual(200);
-        expect(res.body.status).toBe('UP');
+        expect(res.body.status).toBe('DOWN');
     });
 });
